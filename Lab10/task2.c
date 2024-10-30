23ff7408aea4944885b33b77dcdb5365f0986dbe
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-10-30 20:00:03 +0300

    Update task2.c

--- Lab10/task2.c
+++ Lab10/task2.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int StrengLength(char *text);
+void getInput(char **t#include <stdio.h>
+#include <stdlib.h>
+
+int StrengLength(char *text);
 void getInput(char **text, int size);
 void reverseString(char *text, char **reverse, int length);
 
@@ -9,6 +13,7 @@ int main() {
     int a; 
     printf("Enter the size of the string: ");
     scanf("%d", &a);
+    a++;
 
     char *text = malloc(a * sizeof(char));
     if (text == NULL) { 
@@ -52,9 +57,64 @@ void getInput(char **text, int size) {
 }
 
 void reverseString(char *text, char **reverse, int length) {
-    for(int j = 0; j < length; j++) {
+    for(int j = 0; j <= length; j++) {
         (*reverse)[j] = text[length - 1 - j];
     }
     (*reverse)[length] = '\0';
 }
+ext, int size);
+void reverseString(char *text, char **reverse, int length);
 
+int main() {
+    int a; 
+    printf("Enter the size of the string: ");
+    scanf("%d", &a);
+    a++;
+
+    char *text = malloc(a * sizeof(char));
+    if (text == NULL) { 
+        printf("No memory allocated\n");
+        return 1;
+    }
+
+    getInput(&text, a); 
+
+    int length = StrengLength(text); 
+    printf("Length: %d\n", length);
+
+    char *reverse = malloc((length + 1) * sizeof(char));
+    if (reverse == NULL) {
+        printf("No memory allocated for reverse\n");
+        free(text);
+        return 1;
+    }
+
+    reverseString(text, &reverse, length);
+    printf("Reversed: %s\n", reverse);
+
+    free(text);
+    free(reverse);
+
+    return 0;
+}
+
+int StrengLength(char *text) {
+    int count = 0;
+    for (int i = 0; text[i] != '\0' && text[i] != '\n'; i++) {
+        count++;
+    }
+    return count;
+}
+
+void getInput(char **text, int size) {
+    printf("Please enter text: ");
+    getchar();
+    fgets(*text, size, stdin); 
+}
+
+void reverseString(char *text, char **reverse, int length) {
+    for(int j = 0; j <= length; j++) {
+        (*reverse)[j] = text[length - 1 - j];
+    }
+    (*reverse)[length] = '\0';
+}
